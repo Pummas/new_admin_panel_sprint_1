67201911fe4285a83350d0daef4353f283597b91
Pumas <l.a.r.p@icloud.com>
2022-04-26 22:59:13 +0300

    Добавил модель Filmwork

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Genre
+from .models import Genre, Filmwork
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
     pass
--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -1,15 +1,54 @@
 import uuid
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
-class Genre(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField('name', max_length=255)
-    description = models.TextField('description', blank=True)
+class FilmworkTypeChoices(models.TextChoices):
+    movie = 'MVE'
+    tw_show = 'TWS'
+
+
+class TimeStampedMixin(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Genre(UUIDMixin, TimeStampedMixin):
+    name = models.CharField('name', max_length=255, null=False)
+    description = models.TextField('description', blank=True)
 
     class Meta:
         db_table = "content\".\"genre"
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
+
+
+class Filmwork(UUIDMixin, TimeStampedMixin):
+    title = models.CharField('title', max_length=255, null=False)
+    description = models.TextField('description', blank=True)
+    creation_date = models.DateField('creation_date', blank=True)
+    rating = models.FloatField('rating', blank=True,
+                               validators=[MinValueValidator(0),
+                                           MaxValueValidator(100)])
+    type = models.TextField('type',
+                            choices=FilmworkTypeChoices.choices,
+                            null=False
+                            )
+
+    class Meta:
+        db_table = "content\".\"film_work"
+        verbose_name = 'Кинопроизведение'
+        verbose_name_plural = 'Кинопроизведения'
+
+    def __str__(self):
+        return self.title
